ced5d90f43f1cd555906c123d0bdb8b5ee0dd780
Alejandro Uribe <jauriberamirez@gmail.com>
2024-05-16 20:15:39 -0300

    First commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+def main() -> None:
+    """Main function of the program."""
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/sample_test.py
@@ -0,0 +1,6 @@
+from src.main import main  # noqa
+
+
+def test_example():
+    """Test example function."""
+    assert 1 == 1
