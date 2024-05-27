590012683bd8843d237ddc1b7be47db687bfc4d9
Alejandro Uribe <jauriberamirez@gmail.com>
2024-05-26 23:40:09 -0300

    feat: Documentation

--- web/pages/About.py
+++ web/pages/About.py
@@ -5,13 +5,13 @@ from web.utils.pages import make_sidebar
 
 st.set_page_config(
     layout="centered",
-    page_icon=":feet:",
-    page_title="Hot Products",
+    page_icon="ℹ️",
+    page_title="About",
     initial_sidebar_state="collapsed",
 )
 make_sidebar()
 
 with open("README.md") as f:
-    st.markdown(f.read())
+    st.markdown(f.read(), unsafe_allow_html=True)
 
 st.markdown(FOOTER, unsafe_allow_html=True)
