a7109ce32cabdb82443f1e473baf5169876c6eeb
Alejandro Uribe <jauriberamirez@gmail.com>
2024-06-08 21:00:45 -0300

    Web UI, Backend V1 This pull request includes several commits that add new functionality to the project. The changes include:
    - Web UI improvements, such as a new reviews page, sorting feature, and updated product page.
    
    - Dockerization of the application components for easier deployment and scalability.
    
    - Backend routing for communication with the database and web UI.

--- shein/main.py
+++ shein/main.py
@@ -1,20 +1,31 @@
-from shein.constants import COLLECTION_INDEX, DATABASE_NAME
-from shein.scrapper.products.details import get_details
-from shein.scrapper.products.images import download_images
-from shein.scrapper.products.list import list_products
-from shein.scrapper.utils.database import clean_price_column, create_collection_index, get_mongo_database
+from selenium.webdriver.common.by import By
+
+from shein.constants import COLLECTION_DETAILS, DATABASE_NAME
+from shein.scrapper.utils.database import get_mongo_database
 from shein.scrapper.utils.scraper import get_driver
 
 if __name__ == "__main__":
     driver = get_driver()
     mongo_database = get_mongo_database(DATABASE_NAME)
-    create_collection_index(mongo_database, COLLECTION_INDEX)
+    product_details_collection = mongo_database[COLLECTION_DETAILS]
+    pending_urls = product_details_collection.find({"cateogries": {"$exists": False}})
 
-    list_products(driver, mongo_database)
-    get_details(driver, mongo_database)
+    for url in pending_urls:
+        url = url["url"]
+
+        try:
+            driver.get(url)
+            s = driver.find_element(By.CLASS_NAME, "bread-crumb__inner").get_attribute("innerText")
+            s = s.replace("\n", "").split("/")[1:-1]
+
+            product_details_collection.update_one({"url": url}, {"$set": {"cateogries": s}})
+
+        except Exception as e:
+            print(e)
+
     driver.close()
 
-    download_images(mongo_database)
-    clean_price_column(mongo_database)
+    # download_images(mongo_database)
+    # clean_price_column(mongo_database)
 
     mongo_database.client.close()
